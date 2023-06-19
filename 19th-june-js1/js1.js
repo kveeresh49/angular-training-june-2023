b964d1b61f29244492e701c2c77e6df40a6d7cf0
kveeresh49 <koppula.veeresh@gmail.com>
2023-06-19 19:29:33 +0530

    angular day1 class

---
+++ 19th-june-js1/js1.js
@@ -0,0 +1,14 @@
+function ChangeEmployeeDetails() {
+
+    document.getElementById('userName').innerText ="Santoshk";
+    document.getElementById('name').innerText ="santhu";
+    document.getElementById('company').innerText ="Infosis";
+}
+
+
+function ChangeEmployeeDetailsByClass() {
+
+    document.getElementsByClassName('value')[0].innerText ="Santoshk";
+    document.getElementsByClassName('value')[1].innerText ="santhu";
+    document.getElementsByClassName('value')[2].innerText ="Infosis";
+}
